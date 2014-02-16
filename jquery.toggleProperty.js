3cc462880828ec0c17d14cd5bf54987cc099777c
josisusan <josisusan@gmail.com>
2014-02-16 11:37:43 +0545

    Jquery plugin to toggle properties

---
+++ jquery.toggleProperty.js
@@ -0,0 +1,11 @@
+// inspired from http://stackoverflow.com/questions/4702000/toggle-input-disabled-attribute-using-jquery
+(function($){
+  $.fn.toggleProperty = function(property){
+    
+    return this.each( function() {
+      element_dom = $(this);
+      element_dom.prop(property, !element_dom.prop(property));
+    });
+
+  }
+}(jQuery));
